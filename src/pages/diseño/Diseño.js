cdb479d4a6d93769724d69b38f2da31aae7946cb
René Alexander Rivera Hernández <alexxanderrivas@gmail.com>
2022-10-10 12:34:16 -0600

    descripciones terminadas

--- src/pages/diseño/Diseño.js
+++ src/pages/diseño/Diseño.js
@@ -15,26 +15,32 @@ export default function Diseño(){
         {
             id: 1,
             ISrc: contIMG1,
+            descp:'Poryecto academico con el que se practico con la creación de piezas infograficas, para lo cual se dejo tematica libre.',
         },
         {
             id: 2,
             ISrc: contIMG2,
+            descp:'Proyecto academico en el cual se trabajo un personaje para una marca exitente, para dicha actividad se debia crear una pieza publicitaria con dicho personaje.',
         },
         {
             id: 3,
             ISrc: contIMG3,
+            descp:'Proyecto academico en el cual se trabajo creo la portada al que nosotros dariamos nombre.',
         },
         {
             id: 4,
             ISrc: contIMG4,
+            descp:'Proyecto academico de la materia de fotografia aplicada en la cual hariamos uso de una fotgrafia de algun producto que tubieramos en casa y la usariamos para crear una pieza publicitaria.',
         },
         {
             id: 5,
             ISrc: contIMG5,
+            descp:'Proyecto academico en el cual se creo una marca fictisia la cual ofreceria cursos en alguna tematica de nuestro interes, para la cual se crearian una serie de artes publicitarias para la red social de Instagram.',
         },
         {
             id: 6,
             ISrc: contIMG6,
+            desc:'Proyecto academico en el cual se creo una marca fictisia la cual ofreceria cursos en alguna tematica de nuestro interes, para la cual se crearian una serie de artes publicitarias para la red social de Instagram.',
         },
     ]
 
@@ -42,52 +48,63 @@ export default function Diseño(){
         {
             id: 7,
             VSrc: 'https://www.youtube.com/embed/zSNJaWjri0w',
-            titulo: 'Practica 1'
+            titulo: 'Practica 1',
+            descripc: 'Video creado como practica academica para la clase de Diseño grafico digital 5, en el cual se hizo uso de un producto o servicio exitente, para el cual se deberia de crear una videoguia para el uso del producto o servicio.',
         },
         {
             id: 8,
             VSrc: 'https://www.youtube.com/embed/wfn22K51LNM',
-            titulo:'practica 5'
+            titulo:'practica 5',
+            descripc: 'Video creado como practica academica para la clase de Diseño grafico digital 5, en el cual se hizo uso de una marca exitente, para el cual se creo un video promocional en el cual dicha marca presenta alguno de los productos que ofrecen',
         },
         {
             id: 9,
             VSrc: 'https://www.youtube.com/embed/DLC1K-9utsc',
-            titulo:'Practica 4'
+            titulo:'Practica 4',
+            descripc: 'Video creado como pausa publicitaria para un parcial de la materia de diseño grafico digital 5, en la cual se nos pedia hace uso de marcas o productos exitentes, respetando la imagen de las marcas utilizados.',
         },
         {
             id: 10,
             VSrc: 'https://www.youtube.com/embed/0duA4dvVJCw',
-            titulo:'Practica 2'
+            titulo:'Practica 2',
+            descripc: 'Video creado como pausa publicitaria para un parcial de la materia de diseño grafico digital 5, en la cual se debia concienciar a la audiencia sobre las normas de bioseguridad.',
         },
         {
             id: 11,
             VSrc: 'https://www.youtube.com/embed/FUhyawslSLE',
-            titulo:'Parcial 1 Video'
+            titulo:'Parcial 1 Video',
+            descripc: 'Video creado como pausa publicitaria para un parcial de la materia de diseño grafico digital 5, en la cual se nos pedia hace uso de marcas o productos exitentes, respetando la imagen de las marcas utilizados.',
         },
         {
             id: 12,
             VSrc: 'https://www.youtube.com/embed/6KY73ontL6M',
-            titulo:'Spot 3 Parcial DGDV'
+            titulo:'Spot 3 Parcial DGDV',
+            descripc: 'Video creado como pausa publicitaria para un parcial de la materia de diseño grafico digital 5, en la cual se debia concienciar a la audiencia sobre las normas de bioseguridad.',
         },
         {
             id: 13,
             VSrc:'https://www.youtube.com/embed/To9Cp-ggu4E',
-            titulo:'Spot Parcial 1 Video'
+            titulo:'Spot Parcial 1 Video',
+            descripc: 'Video creado como pausa publicitaria para un parcial de la materia de diseño grafico digital 5, en la cual se nos pedia hace uso de marcas o productos exitentes, respetando la imagen de las marcas utilizados.',
         },
     ]
 
     const[view, setview] =useState(false);
     const[showCont, setshowCont] = useState('');
-    const getCont = (ISrc) => {
+    const [texto, settexto] = useState('');
+    const getCont = (ISrc, descp) => {
         setshowCont(ISrc);
         setview(true);
+        settexto(descp);
     };
 
     const [vision, setvision] = useState(false);
     const [showVideo, setshowVideo] = useState('');
-    const getVideo = (VSrc) => {
+    const [text, settext] = useState('');
+    const getVideo = (VSrc, descripc) => {
         setshowVideo(VSrc);
         setvision(true);
+        settext(descripc);
     };
 
     return(
@@ -95,11 +112,13 @@ export default function Diseño(){
         <div className={view ? "view Show" : "view"}>
             <img src={showCont} alt='imagen'/>
             <i class="fa-solid fa-circle-xmark" onClick={()=> setview(false)}></i>
+            <p className='textDesc'>{texto}</p>
         </div>
 
         <div className={vision ? "vision visible" : "vision"}>  
         <iframe src={showVideo + "?autoplay=1"} allow='autoplay;' title={Video.titulo} allowFullScreen></iframe>
             <i class="fa-solid fa-circle-xmark" onClick={()=> setvision(false)}></i>
+            <p className='textDesc'>{text}</p>
         </div>
 
 
@@ -107,16 +126,18 @@ export default function Diseño(){
             <Boton className='Boton'/>
             {imgdata.map((item,index) => {
                 return(
-                    <div className='contenido' key={index} onClick={()=> getCont(item.ISrc)}>
+                    <div className='contenido' key={index} onClick={()=> getCont(item.ISrc,item.descp)}>
                         <img src={item.ISrc} className='content' alt='imagen'/>
+                        <p hidden>{item.descp}</p>
                     </div>
                 )
             })}
             
             {Video.map((item, index) => {
                 return (
-                    <div className='containerV' key={index} onClick={()=> getVideo(item.VSrc)} >
+                    <div className='containerV' key={index} onClick={()=> getVideo(item.VSrc,item.descripc)} >
                         <iframe src={item.VSrc +"?controls=0"} title={item.titulo} className='video'/>
+                        <p hidden>{item.descripc}</p>
                     </div>
                 )
             })}
--- src/pages/fotografia/Fotografia.js
+++ src/pages/fotografia/Fotografia.js
@@ -16,7 +16,7 @@ import img12 from './img/12.JPG';
 import img13 from './img/13.JPG';
 import img14 from './img/14.JPG';
 import img15 from './img/15.JPG';
-import img16 from './img/16.JPG';
+import img16 from './img/16.JPG';   
 import img17 from './img/17.JPG';
 
 import { useState } from "react";
@@ -26,95 +26,113 @@ export default function Fotografia(){
         {
             id: 1,
             imgSrc: img1,
+            desc: 'Fotografia tomada con finalidad academica, dicha actividad consistio en hacer fotografia publicitaria.',
         },
         {
             id: 2,
             imgSrc: img2,
+            desc: 'Esta fotografia fue tomada en la carretera litoral durante la primeras vacaciones fuera de casa despues de la cuarentena obligatoria.',
         },
         {
             id: 3,
             imgSrc: img3,
+            desc: 'Esta fotografia fue tomada en el Parque Naciona de Montecristo, en Metapan, Santa Ana, fue una foto tomada por hobbie personal.',
         },
         {
             id: 4,
             imgSrc: img4,
+            desc: 'Fotografia tomada durante una de las proseciones de la semana santa del año 2022, especificamente el domigo santo.',
         },
         {
             id: 5,
             imgSrc: img5,
+            desc: 'Esta fotografia fue tomada en el Parque Naciona de Montecristo, en Metapan, Santa Ana, Tomada con la finalidad de jugar con el desenfoque del fondo.',
         },
         {
             id: 6,
             imgSrc: img6,
+            desc: 'Fotografia tomada durante una de las proseciones de la semana santa del año 2022, especificamente el domigo de ramos, para la toma de la fotografia me interesaba el jugar con los angulos desde los cuales tomar la foto.',
         },
         {
             id: 7,
             imgSrc: img7,
+            desc: 'Esta fotografia forma parte de un trabajo presentado en la clase de Fotografia aplicada en la cual se uso el concepto de la depresion.',
         },
         {
             id: 8,
             imgSrc: img8,
+            desc: 'Esta fotografia forma parte de un trabajo presentado en la clase de Fotografia aplicada para practicar la toma de fotos tipo retrato.',
         },
         {
             id: 9,
             imgSrc: img9,
+            desc: 'Fotografia tomada como hobbie durante las vacaciones del 2021 en Metapan, la botella era parte de la decoración del corredor frontal de una casa.',
         },
         {
             id: 10,
             imgSrc: img10,
+            desc: 'Fotografia tomada al interior del Parque Nacional de Montecristo, como parte de mi hobbie durante las vacaciones.',
         },
         {
             id: 11,
             imgSrc: img11,
+            desc: 'Fotografia tomada al interior del Parque Nacional de Montecristo, en el jardin botanico de los 100 años, como parte de mi hobbie durante las vacaciones.',
         },
         {
             id: 12,
             imgSrc: img12,
+            desc: 'Fotografia tomada al interior del Parque Nacional de Montecristo, como parte de mi hobbie durante las vacaciones.',
         },
         {
             id: 13,
             imgSrc: img13,
+            desc: 'Fotografia tomada como parte de una practica de la materia de fotografia aplicada, en la que experimentamos con la iluminacion y con la velocidad de obturacion.',
         },
         {
             id: 14,
             imgSrc: img14,
+            desc: 'Fotografia tomada con finalidad academica, dicha actividad consistio en hacer fotografia publicitaria en esta ocacion se trata de un formato vertical.',
         },
         {
             id: 15,
             imgSrc: img15,
+            desc: 'fotografia que forma parte de un parcial de fotografia aplicada, en la cual se uso el concepto de naturaleza belica, para esta foto se jugo con la iluminacion del fondo y el objeto a fotografiar.',
         },
         {
             id: 16,
             imgSrc: img16,
+            desc: 'fotografia que forma parte de un parcial de fotografia aplicada, en la cual se uso el concepto de naturaleza belica, para esta foto se jugo con la iluminacion del fondo y el objeto a fotografiar.',
         },
         {
             id: 17,
             imgSrc: img17,
+            desc: 'fotografia que forma parte de un parcial de fotografia aplicada, en la cual se uso el concepto de naturaleza belica, para esta foto se agrearon mas objetos para acompañar al elemento principal.',
         },
     ]
-
     const [model, setmodel] = useState(false);
     const[showImg, setShowImg] = useState('');
-    const getImg = (imgSrc) =>{
+    const [text, settext] = useState('');
+    const getImg = (imgSrc,desc) =>{
         setShowImg(imgSrc);
         setmodel(true);
+        settext(desc);
     }
-
-    
 
     return(
         <>
         <div className={model? "model open" : "model"}>
             <img src={showImg} alt='imagen'/>
             <i class="fa-solid fa-circle-xmark" onClick={() => setmodel(false)}></i>
+            <p className="descrito">{text}</p>
         </div>
         
         <div className="containerGalery">
             <Boton className='Boton'/>
             {data.map((item,index) => {
                 return(
-                    <div className="fotos" key={index} onClick={()=> getImg(item.imgSrc)}>
+                    <div className="fotos" key={index} onClick={()=> getImg(item.imgSrc,item.desc)}>
                         <img src={item.imgSrc} className='imgs' alt='imagen'/>
+                        <p hidden>{item.desc}</p>
                     </div>
                 )
             })}
