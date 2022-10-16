9cae169bc0a4c1240ae17206cf00260e90e582b7
René Alexander Rivera Hernández <alexxanderrivas@gmail.com>
2022-10-16 17:13:06 -0600

    retrosesoFormulario

--- src/pages/contact/Contacto.js
+++ src/pages/contact/Contacto.js
@@ -1,54 +1,8 @@
-import { useState } from 'react'
 import logo from '../landing/logoF.png'
 import './contacto.scss'
-import { isEmpty, isEmail } from 'validator'
 
 export default function contacto() {
-    const [error, setError] = useState({
-        estado: false,
-        mensaje: '',
-    })
-    const [formulario, setFormulario] = useState({
-        name: "",
-        email: "",
-        message: "",
-    })
 
-    function capturaDatos(e) {
-        setFormulario({
-            ...formulario,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    function validarDatos(e) {
-        const { name, email, message } = formulario;
-        e.preventDefault();
-
-        if (isEmpty(name) || isEmpty(email) || isEmpty(message)) {
-            setError({
-                estado: true,
-                mensaje: 'Complete todos los campos en el formulario'
-            })
-        } else {
-            if (!isEmail(email)) {
-                setError({
-                    estado: true,
-                    mensaje: 'escribe un correo valido'
-                })
-            } else {
-                setError({
-                    estado: false,
-                    mensaje: '',
-                })
-                setFormulario({
-                    name: "",
-                    email: "",
-                    message: "",
-                })
-            }
-        }
-    }
 
     return (
         <>
@@ -59,39 +13,34 @@ export default function contacto() {
                 <div className="contenedorContacto">
                     <img src={logo} alt="logo" />
                     <div className='contenedorFormulario'>
-                        <form name='contacto' method='POST' netlify onSubmit={validarDatos} action='/'>
+                        <form name='contacto' method='POST' netlify onSubmit='submit' action='/'>
 
-                            <input type='hidden' name='form-name' value='contacto' />
-
-                            <div>
-                                <label>Nombre <br />
-                                    <input type='text' name='name' className='areaInput input' placeholder='¿Cómo debo llamarte?' id='Nombre' onChange={capturaDatos} value={formulario.name} />
-                                </label>
-                            </div>
+                            <input type='hidden' name='form-name' value='contacto'/>
 
                             <div>
-                                <label>Correo <br />
-                                    <input type='email' name='email' className='areaInput input' placeholder='Esta información queda entre tú y yo' id='Correo' onChange={capturaDatos} value={formulario.email} />
+                                <label>Nombre <br/>
+                                    <input type='text' name='name' required className='areaInput input' placeholder='¿Cómo debo llamarte?'/>
                                 </label>
                             </div>
-
+                            
+                            <div>
+                                <label>Correo <br/>
+                                    <input type='email' name='email' required className='areaInput input' placeholder='Esta información queda entre tú y yo'/>
+                                </label>
+                            </div>
+                            
 
                             <div>
-                                <label>Comentarios <br />
-                                    <textarea name='message' className='areaInput text' placeholder='Dime que opinas…' id='Mensaje' onChange={capturaDatos} value={formulario.message}></textarea>
+                                <label>Comentarios <br/>
+                                    <textarea name='message' required className='areaInput text' placeholder='Dime que opinas… '></textarea>
                                 </label>
                             </div>
-
-                            {error.estado &&
-                                <div className='error'>
-                                    <span>Error:</span>  {error.mensaje}
-                                </div>}
 
                             <button type='submit'>Enviar</button>
                         </form>
                     </div>
                 </div>
-            </div>
+            </div> 
         </>
     )
 }
