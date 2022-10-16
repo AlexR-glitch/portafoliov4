d4c95065243415dbd9445420b978253b439cb21d
René Alexander Rivera Hernández <alexxanderrivas@gmail.com>
2022-10-16 17:07:33 -0600

    Update Contacto.js

--- src/pages/contact/Contacto.js
+++ src/pages/contact/Contacto.js
@@ -1,52 +1,50 @@
+import { useState } from 'react'
 import logo from '../landing/logoF.png'
 import './contacto.scss'
-import { isEmpty, isEmail} from 'validator';
-import { useState } from 'react';
+import { isEmpty, isEmail } from 'validator'
 
 export default function contacto() {
-
-    const [error, seterror] = useState({
+    const [error, setError] = useState({
         estado: false,
         mensaje: '',
-    });
-
-    const [formulario, setformulario] = useState({
-        name:"",
-        email:"",
-        message:"",
-    });
+    })
+    const [formulario, setFormulario] = useState({
+        name: "",
+        email: "",
+        message: "",
+    })
 
     function capturaDatos(e) {
-        setformulario({
+        setFormulario({
             ...formulario,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
-    } 
+    }
 
-    function validarDatos (e){
-        const {name, email, message} = formulario;
+    function validarDatos(e) {
+        const { name, email, message } = formulario;
         e.preventDefault();
 
         if (isEmpty(name) || isEmpty(email) || isEmpty(message)) {
-            seterror({
+            setError({
                 estado: true,
                 mensaje: 'Complete todos los campos en el formulario'
             })
-        }else {
-            if(!isEmail(email)){
-                seterror({
+        } else {
+            if (!isEmail(email)) {
+                setError({
                     estado: true,
                     mensaje: 'escribe un correo valido'
                 })
             } else {
-                seterror({
+                setError({
                     estado: false,
                     mensaje: '',
                 })
-                setformulario({
-                    name:"",
-                    email:"",
-                    message:"",
+                setFormulario({
+                    name: "",
+                    email: "",
+                    message: "",
                 })
             }
         }
@@ -63,37 +61,37 @@ export default function contacto() {
                     <div className='contenedorFormulario'>
                         <form name='contacto' method='POST' netlify onSubmit={validarDatos} action='/'>
 
-                            <input type='hidden' name='form-name' value='contacto'/>
+                            <input type='hidden' name='form-name' value='contacto' />
 
                             <div>
-                                <label>Nombre <br/>
-                                    <input type='text' name='name' className='areaInput input' placeholder='¿Cómo debo llamarte?' id='Nombre' onChange={capturaDatos} value={formulario.name}/>
+                                <label>Nombre <br />
+                                    <input type='text' name='name' className='areaInput input' placeholder='¿Cómo debo llamarte?' id='Nombre' onChange={capturaDatos} value={formulario.name} />
                                 </label>
                             </div>
-                            
-                            <div>
-                                <label>Correo <br/>
-                                    <input type='email' name='email' className='areaInput input' placeholder='Esta información queda entre tú y yo' id='Correo' onChange={capturaDatos} value={formulario.email}/>
-                                </label>
-                            </div>
-                            
 
                             <div>
-                                <label>Comentarios <br/>
+                                <label>Correo <br />
+                                    <input type='email' name='email' className='areaInput input' placeholder='Esta información queda entre tú y yo' id='Correo' onChange={capturaDatos} value={formulario.email} />
+                                </label>
+                            </div>
+
+
+                            <div>
+                                <label>Comentarios <br />
                                     <textarea name='message' className='areaInput text' placeholder='Dime que opinas…' id='Mensaje' onChange={capturaDatos} value={formulario.message}></textarea>
                                 </label>
                             </div>
-                            
-                            {error.estado && 
-                            <div className='error'>
-                                <span>Error:</span>  {error.mensaje}
-                            </div>}
+
+                            {error.estado &&
+                                <div className='error'>
+                                    <span>Error:</span>  {error.mensaje}
+                                </div>}
 
                             <button type='submit'>Enviar</button>
                         </form>
                     </div>
                 </div>
-            </div> 
+            </div>
         </>
     )
 }
