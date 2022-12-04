361072dc6b6bcf41940a1da50613c5acdad9a9a8
René Alexander Rivera Hernández <alexxanderrivas@gmail.com>
2022-12-04 16:46:57 -0600

    version final CTM

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,6 +11,13 @@ export default function Footer(){
                     Derechos reservados {Year} | <strong> Portafolio profesional y Académico </strong> | René Rivera
                 </p>
             </div>
+            <div className="RRSS">
+                <a href="https://www.instagram.com/alex_r.97/" className="RS">Instagram</a>
+                <span> | </span>
+                <a href="https://www.facebook.com/rene.alexander.rivera.hernandz" className="RS">Facebook</a>
+                <span> | </span>
+                <a href="https://www.linkedin.com/feed/" className="RS">Linkedin</a>
+            </div>
         </div>
         </>
     )
